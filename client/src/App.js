08f140b0cbdc049c0cae6cd06d29b41283e9126a
yeongg0223 <yeongg0223@gmail.com>
2024-12-23 17:49:40 +0900

    캘린더 height 수정

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calendar from 'react-calendar';
 import '../node_modules/icheck-material/icheck-material.min.css';
+import moment from 'moment';
 
 function App() {
   return (
@@ -15,35 +16,46 @@ function App() {
 }
 
 function Home() {
-  const [date, setDate] = useState(new Date()); // 날짜 상태 추가
+  const [date, setDate] = useState(new Date());
 
   const onChange = (newDate) => {
     setDate(newDate);
   };
 
+  const formatDay = (locale, date) => {
+    return date.getDate(); 
+  };
+
+  const formatMonthYear = (locale, date) => {
+    return moment(date).format("MMMM, YYYY"); // 예: "December, 2024"
+  };
+
   return (
-    <div class='main'>
-      <div id='sidebar'>
-        <div class='profile'>
-          <img src='https://avatars.githubusercontent.com/u/144537092?v=4&size=64' class='profile-image' />
-          <div class='profile-info'>
-            <span class='name'>김영혜</span>
-            <span class='state'>집에가고싶다</span>
+    <div className="main">
+      <div id="sidebar">
+        <div className="profile">
+          <img src="https://avatars.githubusercontent.com/u/144537092?v=4&size=64" className="profile-image" />
+          <div className="profile-info">
+            <span className="name">김영혜</span>
+            <span className="state">집에가고싶다</span>
           </div>
         </div>
-        <hr/>
-        <div class='todolist'>
+        <hr />
+        <div className="todolist">
           <h3>오늘할일</h3>
-          <div class="icheck-material-green">
-            <input type="checkbox" name='dinner' id='green' checked/>
-            <label for='green'>저녁밥</label>
+          <div className="icheck-material-green">
+            <input type="checkbox" name="dinner" id="green" checked />
+            <label htmlFor="green">저녁밥</label>
           </div>
-          
-          
         </div>
       </div>
-      <div id='calendar'>
-        <Calendar onChange={onChange} value={date} />
+      <div id="calendar">
+      <Calendar
+          onChange={onChange}
+          value={date}
+          formatDay={formatDay}
+          formatMonthYear={formatMonthYear} // 여기서 월/년 형식을 변경
+        />
         {/* <p>Selected Date: {date.toDateString()}</p>  */}
       </div>
     </div>
